8aa0f81d47af546c94609bb7e73d8a9ea9f362a3
DW5604 <wangdavid5604@gmail.com>
2022-09-06 23:49:34 +1000

    ace logic added

--- script.js
+++ script.js
@@ -66,28 +66,6 @@ var dealerCard1 = dealerCards.push(shuffledDeck.pop());
 var dealerCard2 = dealerCards.push(shuffledDeck.pop());
 console.log(dealerCards);
 
-// Show Player's Cards
-var returnPlayersHand = function (playerCards) {
-  var index = 0;
-  var noOfPlayerCards = playerCards.length;
-  while (index < noOfPlayerCards) {
-    myOutputValue = `Player drew ${playerCards[index].name} of ${playerCards[index].suit}. `;
-    index = index + 1;
-  }
-  return myOutputValue;
-};
-
-// Show Dealer's Cards
-var returnDealersHand = function (dealerCards) {
-  var index = 0;
-  var noOfDealerCards = dealerCards.length;
-  while (index < noOfDealerCards) {
-    myOutputValue = `Player drew ${dealerCards[index].name} of ${dealerCards[index].suit}. `;
-    index = index + 1;
-  }
-  return myOutputValue;
-};
-
 // Sum of Player Cards
 var returnSumofPlayerCards = function (playerCards) {
   var index = 0;
@@ -95,6 +73,7 @@ var returnSumofPlayerCards = function (playerCards) {
   var playerCardsLength = playerCards.length;
   while (index < playerCardsLength) {
     var currentPlayerCard = playerCards[index];
+    // JQK
     if (
       currentPlayerCard.name == "jack" ||
       currentPlayerCard.name == "queen" ||
@@ -102,7 +81,12 @@ var returnSumofPlayerCards = function (playerCards) {
     ) {
       sumOfPlayerCards = sumOfPlayerCards + 10;
     } else {
-      sumOfPlayerCards = sumOfPlayerCards + currentPlayerCard.rank;
+      // Ace
+      if (currentPlayerCard.name == "ace" && sumOfPlayerCards < 11) {
+        sumOfPlayerCards = sumOfPlayerCards + 11;
+      } else {
+        sumOfPlayerCards = sumOfPlayerCards + currentPlayerCard.rank;
+      }
     }
     index = index + 1;
   }
@@ -117,14 +101,20 @@ var returnSumOfDealerCards = function (dealerCards) {
   var sumOfDealerCards = 0;
   while (index < dealerCardsLength) {
     var currentDealerCard = dealerCards[index];
+    // JQK
     if (
       currentDealerCard.name == "jack" ||
       currentDealerCard.name == "queen" ||
       currentDealerCard.name == "king"
     ) {
-      currentDealerCard = currentDealerCard + 10;
+      sumOfDealerCards = sumOfDealerCards + 10;
     } else {
-      sumOfDealerCards = sumOfDealerCards + currentDealerCard.rank;
+      // Ace
+      if (currentDealerCard.name == "ace" && sumOfDealerCards < 11) {
+        sumOfDealerCards = sumOfDealerCards + 11;
+      } else {
+        sumOfDealerCards = sumOfDealerCards + currentDealerCard.rank;
+      }
     }
     index = index + 1;
   }
@@ -132,42 +122,107 @@ var returnSumOfDealerCards = function (dealerCards) {
   return sumOfDealerCards;
 };
 
-var initialHand = function (dealerCardSum, playerCardSum) {
-  // Winner is dependant on sum of card rank
-  var playerCardSum = returnSumofPlayerCards(playerCards);
-  console.log(playerCardSum);
-  var dealerCardSum = returnSumOfDealerCards(dealerCards);
-  console.log(dealerCardSum);
+var playerCardSum = returnSumofPlayerCards(playerCards);
+var dealerCardSum = returnSumOfDealerCards(dealerCards);
 
+var initialHand = function (dealerCardSum, playerCardSum) {
+  // Calculate sum of initial cards
+  var playerCardSum = returnSumofPlayerCards(playerCards);
+  var dealerCardSum = returnSumOfDealerCards(dealerCards);
+
+  // Return initial cards
   var returnPlayerCard1 = playerCards[0];
   var returnPlayerCard2 = playerCards[1];
   var returnDealerCard1 = dealerCards[0];
   var returnDealerCard2 = dealerCards[1];
 
   myOutputValue = `Player hand: ${returnPlayerCard1.name} of ${returnPlayerCard1.suit}, ${returnPlayerCard2.name} of ${returnPlayerCard2.suit}. Dealer hand: ${returnDealerCard1.name} of ${returnDealerCard1.suit}, ${returnDealerCard2.name} of ${returnDealerCard2.suit}. `;
-
+  // Both 21
   if ((dealerCardSum == playerCardSum) == 21) {
-    myOutputValue = myOutputValue + `It's a tie`;
-  } else if (dealerCardSum == 21) {
-    myOutputValue = myOutputValue + `Dealer wins by Blackjack.`;
-  } else if (playerCardSum == 21) {
-    myOutputValue = myOutputValue + `Player wins by Blackjack.`;
-  } else if (playerCardSum < 21) {
+    myOutputValue = myOutputValue + ` It's a tie`;
+  }
+  // Blackjack for dealer
+  else if (dealerCardSum == 21) {
+    myOutputValue = myOutputValue + ` Dealer wins by Blackjack.`;
+  }
+  // Blackjack for player
+  else if (playerCardSum == 21) {
+    myOutputValue = myOutputValue + ` Player wins by Blackjack.`;
+  }
+  // Player sum < 21
+  else if (playerCardSum < 21) {
     myOutputValue =
-      myOutputValue + `Type "hit" or "stand" to continue with the game`;
+      myOutputValue + ` Type "hit" or "stand" to continue with the game`;
   }
   currentGameMode = "hitorstand";
   return myOutputValue;
 };
 
+// Show Player's Cards that is dea
+var returnPlayersHand = function (playerCards) {
+  var index = 0;
+  var noOfPlayerCards = playerCards.length;
+  while (index < noOfPlayerCards) {
+    myOutputValue = `Player drew ${playerCards[index].name} of ${playerCards[index].suit}. `;
+    index = index + 1;
+  }
+  console.log(myOutputValue);
+  return myOutputValue;
+};
+
+// Hit or Stand
 var hitOrStand = function (input) {
-  if (input == `stand`) {
-    myOutputvalue = `You have chosen to stand. It is now the dealer's turn.`;
-  } else if (input == `hit`) {
+  // Hit
+  if (input == `hit`) {
+    // Create new card for every hit
     playerCards.push(shuffledDeck.pop());
-    myOutputValue =
-      returnPlayersHand(playerCards) +
-      `Sum of player cards is now ${returnSumofPlayerCards(playerCards)}.`;
+    myOutputValue = `${returnPlayersHand(
+      playerCards
+    )} Sum of player's cards is now ${returnSumofPlayerCards(playerCards)}.`;
+    // Sum < 21
+    if (returnSumofPlayerCards(playerCards) < 21) {
+      myOutputValue = myOutputValue + ` Type "hit" or "stand" to continue`;
+      console.log(myOutputValue);
+    }
+    // Sum = 21
+    else if (returnSumofPlayerCards(playerCards) == 21) {
+      myOutputValue = myOutputValue + ` Press 'stand' to continue.`;
+      console.log(myOutputValue);
+    }
+    // Sum > 21
+    else if (returnSumofPlayerCards(playerCards) > 21) {
+      myOutputValue =
+        myOutputValue +
+        ` The sum of your cards exceeds 21. Press "stand" to continue.`;
+      console.log(myOutputValue);
+    }
+    // Stand
+  } else if (input == `stand`) {
+    playerCardSum = returnSumofPlayerCards(playerCards);
+    dealerCardSum = returnSumOfDealerCards(dealerCards);
+    // Dealer hits whenever total < 17
+    while (dealerCardSum < 17) {
+      dealerCards.push(shuffledDeck.pop());
+      dealerCardSum = returnSumOfDealerCards(dealerCards);
+    }
+    myOutputValue = `You have chosen to stand. It is now the dealer's turn. Sum of dealer's cards ended up as ${returnSumOfDealerCards(
+      dealerCards
+    )}.`;
+    // Outcomes
+    if (
+      (playerCardSum > 21 && dealerCardSum > 21) ||
+      dealerCardSum == playerCardSum
+    ) {
+      myOutputValue = myOutputValue + " It's a draw!";
+    } else if (dealerCardSum > 21 && playerCardSum < 21) {
+      myOutputValue = myOutputValue + " Player wins!";
+    } else if (dealerCardSum < 21 && playerCardSum > 21) {
+      myOutputValue = myOutputValue + " Dealer wins!";
+    } else if (dealerCardSum > playerCardSum) {
+      myOutputValue = myOutputValue + " Dealer wins!";
+    } else if (playerCardSum > dealerCardSum) {
+      myOutputValue = myOutputValue + " Player wins!";
+    }
   }
   return myOutputValue;
 };
